b9a4a6c26db06e464422842b707f655abf86a8af
jatinder <jatinder@codestoresolutions.com>
2022-08-20 21:39:32 +0530

    employee domian class library added

---
+++ WebApplication1/Models/GetEmployee/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.GetEmployee
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
